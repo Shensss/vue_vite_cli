74ec72253eb6b2a58e9a23b7e4029c511344f5d6
s651489571 <s651489571@163.com>
2021-06-16 17:55:38 +0800

    s

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { lifeCycle, render } from './life-cycle'
 
-createApp(App).mount('#app')
+/**
+ * @name 导出微应用生命周期
+ */
+const {
+  bootstrap,
+  mount,
+  unmount
+} = lifeCycle()
+export { bootstrap, mount, unmount }
+
+/**
+ * @name 单独环境直接实例化vue
+ */
+const __qiankun__ = window.__POWERED_BY_QIANKUN__
+__qiankun__ || render()
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const { name } = require('./package')
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  },
+  configureWebpack: {
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`
+    }
+  }
+}
